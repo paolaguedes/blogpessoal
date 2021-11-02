7dee6ee7be2ebad4c71a75f2572a31febe72c3c7
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-11-01 23:00:38 -0300

    dark mode toggle

---
+++ main.js
@@ -0,0 +1,16 @@
+let open = document.querySelector(".open")
+let close = document.querySelector(".close")
+let body = document.body
+let btn = document.querySelector(".button-default")
+let btn2 = document.querySelector("#portifolio-btn")
+
+function themeDark(){
+  open.classList.toggle('ativo')
+  close.classList.toggle('ativo')
+  body.classList.toggle('ativo')
+  btn.classList.toggle('ativo')
+  btn2.classList.toggle('ativo')
+}
+
+close.addEventListener('click', themeDark )
+open.addEventListener('click', themeDark )
